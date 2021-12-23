d9d04642fdfc426dbf1c3d40e4ca66f844ca9952
NodePHP-Team <96573383+NodePHP-Team@users.noreply.github.com>
2021-12-23 10:17:41 +0000

    Creation of the basic library

---
+++ vendor/NodePHP/Javascript.php
@@ -0,0 +1,22 @@
+<?php
+  
+  namespace NodePHP;
+  
+  class Javascript
+  {
+    const INSTALl_DIR = "vendor/NodePHP/"; //change if you intend to use outside the vendor directory.
+    
+    public function __construct()
+    {
+      
+    }
+    public function evaluate($jsCode , $filename = 'inline:')
+    {
+       
+    }
+    
+  }
+
+  
+
+?>
